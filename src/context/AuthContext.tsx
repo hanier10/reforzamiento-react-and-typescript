13a4ba894b342a075a4907f50cf9d1a3cbd307a3
Hanier Morales <hsmp16@gmail.com>
2025-01-03 23:04:19 -0600

    add-enumeraciones-y-estado

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,27 @@
-import { createContext, PropsWithChildren, useContext } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+enum AuthStatus {
+  "checking" = "checking",
+  "authenticated" = "authenticated",
+  "unauthenticated" = "unauthenticated",
+}
+
+interface User {
+  name: string;
+  email: string;
+}
 
 interface AuthState {
-  hola: string;
+  status: AuthStatus;
+  token?: string;
+  isChecking: boolean;
+  user?: User;
 }
 
 export const AuthContext = createContext({} as AuthState);
@@ -9,10 +29,20 @@ export const AuthContext = createContext({} as AuthState);
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [status, setStatus] = useState(AuthStatus.checking);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(AuthStatus.unauthenticated);
+    }, 1500);
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
-        hola: "Mundo",
+        status: status,
+
+        isChecking: status === AuthStatus.checking,
       }}
     >
       {children}
